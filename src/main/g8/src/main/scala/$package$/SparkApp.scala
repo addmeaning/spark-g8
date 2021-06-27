61ac60605f41a356559b3a5a7c2ffadaded9818b
Antoni Łastowka <addmeaning@gmail.com>
2021-06-27 17:17:13 +0200

    updated template and fixed small bugs

--- src/main/g8/src/main/scala/$package$/SparkApp.scala
+++ src/main/g8/src/main/scala/$package$/SparkApp.scala
@@ -1,7 +1,9 @@
 package $package$
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 object SparkApp extends App {
   val session = SparkSession.builder()
     .appName("spark-app").master("local[*]").getOrCreate()
-
+  import session.implicits._
+  session.close()
 }
